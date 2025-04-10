f1dcbbbb3681ae5a268bdf35a74e304fb41ee053
Lucas_397 <lu.machado.m397@gmail.com>
2025-04-10 12:07:12 -0300

    fix/carrosell && create/product-with-description

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     const swiper = new Swiper('.swiper', {
-        slidesPerView: 3,
+        slidesPerView: 5,
         spaceBetween: 20,
+        slidesPerGroup: 5,
+        loop: true,
         navigation: {
         nextEl: ".arrow-next",
         prevEl: ".arrow-back",
         },
-    });
-});
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            type: "bullets",
+        },
+})});
